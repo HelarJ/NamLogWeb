fab96aa5313463ff48be63f6e501aea94c683087
Helar Jaadla <helar@jaadla.com>
2024-04-25 23:40:29 +0300

    functions

--- dist/nam-log-web/_worker.js
+++ dist/nam-log-web/_worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
         const url = new URL(request.url);
         if (url.pathname.startsWith('/api/log')) {
-            return new Response('Ok');
+            return new Response('Ok ' + request);
         }
         return env.ASSETS.fetch(request);
     },
