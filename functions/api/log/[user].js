d35937e26090b87417e4924306b345d5b213c65b
Helar Jaadla <helar@jaadla.com>
2024-04-25 23:17:22 +0300

    functions

--- functions/api/log/[user].js
+++ functions/api/log/[user].js
@@ -1,7 +1,4 @@
-export function onRequestGet(context) {
-  return new Response(
-    `${context.params.user} ${JSON.stringify(context.params)} ${JSON.stringify(
-      context
-    )}`
-  );
+export async function onRequestGet(context) {
+  console.log(context);
+  return new Response(`${context.params.user}`);
 }
