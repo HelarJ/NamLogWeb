208f1cd815eb79f421afc9f010b0736569b3ef7b
Helar Jaadla <helar@jaadla.com>
2024-04-26 00:05:48 +0300

    functions

--- src/_worker.js
+++ src/_worker.js
@@ -1,9 +1,14 @@
 export default {
-    async fetch(request, env) {
-        const url = new URL(request.url);
-        if (url.pathname.startsWith('/api/log')) {
-            return new Response('Ok ' + request);
-        }
-        return env.ASSETS.fetch(request);
-    },
-}
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/api/log')) {
+      const size = url.searchParams.get('size');
+      const startIndex = url.searchParams.get('startIndex');
+
+      return new Response(
+        `Ok ${size} ${startIndex} ${JSON.stringify(request)}`
+      );
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
